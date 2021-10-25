fc06277ec04fc81a986c494b4ef35ccf3d1d97ed
tokyo-san <92912419+tokyo-san@users.noreply.github.com>
2021-10-25 10:06:11 +0600

    Updating site

---
+++ script.js
@@ -0,0 +1,7 @@
+let body = document.querySelector('body');
+let themeButton = document.querySelector('.theme-button');
+
+themeButton.onclick = function()	{
+	body.classList.toggle('dark-theme');
+	body.classList.toggle('light-theme');
+};
